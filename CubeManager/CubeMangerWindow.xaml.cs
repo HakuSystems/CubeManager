cfe2ac0410ff34d62b6beabe223a89f8ee38e82b
lyze <noah@beuge.eu>
2023-06-11 19:59:51 +0200

    idk why i removed the level up animation but there it is agai

--- CubeManager/CubeMangerWindow.xaml.cs
+++ CubeManager/CubeMangerWindow.xaml.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using CubeManager.Controls;
 using CubeManager.Helpers;
 
@@ -69,8 +71,53 @@ public partial class CubeMangerWindow : Window
 
     private void LevelUp()
     {
-        //...
-        _logger.Debug("LevelUp() executed");
+        var growAnimation = new DoubleAnimation
+        {
+            From = 1,
+            To = 1.5,
+            Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+            AutoReverse = true,
+        };
+        LvlTxtBox.RenderTransform = new ScaleTransform();
+        LvlTxtBox.RenderTransformOrigin = new Point(0.5, 0.5);
+        LvlTxtBox.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, growAnimation);
+        LvlTxtBox.RenderTransform.BeginAnimation(ScaleTransform.ScaleYProperty, growAnimation);
+
+        var originalBrush = LvlTxtBox.Foreground as SolidColorBrush;
+        var animatableBrush = new SolidColorBrush(originalBrush!.Color);
+        LvlTxtBox.Foreground = animatableBrush;
+
+        var colorAnimation = new ColorAnimation
+        {
+            From = originalBrush.Color,
+            To = Colors.Red,
+            Duration = new Duration(TimeSpan.FromSeconds(0.4)),
+            AutoReverse = true,
+        };
+        animatableBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
+
+
+        var spinAnimation = new DoubleAnimation
+        {
+            From = 0,
+            To = 360,
+            Duration = new Duration(TimeSpan.FromSeconds(1))
+        };
+        Sparkles.RenderTransform = new RotateTransform();
+        Sparkles.RenderTransformOrigin = new Point(0.5, 0.5);
+        Sparkles.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, spinAnimation);
+
+
+        var flashAnimation = new DoubleAnimation
+        {
+            From = 1,
+            To = 0,
+            Duration = new Duration(TimeSpan.FromSeconds(0.1)),
+            AutoReverse = true,
+            RepeatBehavior = new RepeatBehavior(5)
+        };
+        ScoreBoardButton.Opacity = 1;
+        ScoreBoardButton.BeginAnimation(Button.OpacityProperty, flashAnimation);
     }
 
     private void LvlProgbar_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
