a8688d64ec3b5732afc4788cf1d086c0ef72861a
lyze <66133638+HakuSystems@users.noreply.github.com>
2023-11-19 19:03:35 +0100

    Refactor Logger functionality for simplicity and efficiency
    In this commit, multiple changes were made to `Logger.cs` to simplify code and improve efficiency. The complexity of certain methods such as `GetLogColor` and `DeleteOldLogs` was reduced by implementing cleaner switch syntax and LINQ respectively. Additionally, the explicit checking for directory existence before creation was removed, relying on `Directory.CreateDirectory` since it doesn't cause an error if a directory already exists. All the public logging methods were refactored to use expression bodied members for brevity. Logging error handling was removed for simplicity and to improve the program's performance.

--- CubeManager/Helpers/Logger.cs
+++ CubeManager/Helpers/Logger.cs
@@ -10,7 +10,7 @@ public class Logger
 
     public Logger()
     {
-        if (!Directory.Exists(LogDirectory)) Directory.CreateDirectory(LogDirectory);
+        Directory.CreateDirectory(LogDirectory);
         //AllocConsole();
         DeleteOldLogs();
     }
@@ -19,20 +19,16 @@ public class Logger
     [DllImport("Kernel32")]
     private static extern void AllocConsole();
 
-    private static ConsoleColor GetLogColor(LogSeverity severity)
+    private static ConsoleColor GetLogColor(LogSeverity severity) => severity switch
     {
-        return severity switch
-        {
-            LogSeverity.Error => ConsoleColor.Red,
-            LogSeverity.ErrSilent => ConsoleColor.Red,
-            LogSeverity.Critical => ConsoleColor.DarkRed,
-            LogSeverity.Warning => ConsoleColor.Yellow,
-            LogSeverity.PrioInfo => ConsoleColor.Cyan,
-            LogSeverity.Info => ConsoleColor.White,
-            LogSeverity.Debug => ConsoleColor.DarkGray,
-            _ => ConsoleColor.White
-        };
-    }
+        LogSeverity.Error or LogSeverity.ErrSilent => ConsoleColor.Red,
+        LogSeverity.Critical => ConsoleColor.DarkRed,
+        LogSeverity.Warning => ConsoleColor.Yellow,
+        LogSeverity.PrioInfo => ConsoleColor.Cyan,
+        LogSeverity.Info => ConsoleColor.White,
+        LogSeverity.Debug => ConsoleColor.DarkGray,
+        _ => ConsoleColor.White
+    };
 
     private static void Log(LogSeverity severity, string message)
     {
@@ -51,66 +47,26 @@ public class Logger
     {
         var logFile = Path.Combine(LogDirectory, $"log-{DateTime.Today:yyyy-MM-dd}.log");
 
-
-        for (var i = 0; i < 10; i++)
-            try
-            {
-                using var fs = new FileStream(logFile, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
-                break;
-            }
-            catch (IOException)
-            {
-                Thread.Sleep(100);
-            }
-
         using var writer = new StreamWriter(logFile, true);
         writer.WriteLine(message);
     }
 
     private static void DeleteOldLogs()
     {
-        var files = Directory.GetFiles(LogDirectory)
-            .Where(x => Path.GetFileName(x).StartsWith("log"))
-            .OrderByDescending(x => File.GetCreationTime(x)).ToArray();
-        for (var i = 0; i < files.Length; i++)
-            if (i > 1)
-                File.Delete(files[i]);
+        var files = Directory.GetFiles(LogDirectory, "log*.log")
+            .OrderByDescending(File.GetCreationTime).Skip(2).ToList();
+
+        files.ForEach(File.Delete);
     }
 
-    public void Info(string message)
-    {
-        Log(LogSeverity.Info, message);
-    }
-
-    public void Warn(string message)
-    {
-        Log(LogSeverity.Warning, message);
-    }
-
-    public void Error(string message)
-    {
-        Log(LogSeverity.Error, message);
-    }
-
-    public void Debug(string message)
-    {
-        Log(LogSeverity.Debug, message);
-    }
-
-    public void PrioInfo(string message)
-    {
-        Log(LogSeverity.PrioInfo, message);
-    }
-
-    public void Critical(string message)
-    {
-        Log(LogSeverity.Critical, message);
-    }
-
-    public void ErrSilent(string message)
-    {
-        Log(LogSeverity.ErrSilent, message);
-    }
+    public void Info(string message) => Log(LogSeverity.Info, message);
+    public void Warn(string message) => Log(LogSeverity.Warning, message);
+    public void Error(string message) => Log(LogSeverity.Error, message);
+    public void Debug(string message) => Log(LogSeverity.Debug, message);
+    public void PrioInfo(string message) => Log(LogSeverity.PrioInfo, message);
+    public void Critical(string message) => Log(LogSeverity.Critical, message);
+    public void ErrSilent(string message) => Log(LogSeverity.ErrSilent, message);
+    
 
     private enum LogSeverity
     {
